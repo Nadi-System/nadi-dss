f19d76193464c135a2e0654164464fb9f5b28196
Gaurav Atreya <allmanpride@gmail.com>
2025-09-22 23:53:12 -0400

    Read dss series into a node

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
+#![allow(improper_ctypes)]
 use nadi_core::nadi_plugin::nadi_plugin;
 
 #[nadi_plugin]
 mod dss {
     use hecdss_sys::*;
-    use nadi_core::nadi_plugin::env_func;
+    use nadi_core::abi_stable::std_types::{RNone, ROption, RSome};
+    use nadi_core::nadi_plugin::{env_func, node_func};
+    use nadi_core::prelude::*;
+    use nadi_core::timeseries::{MaskedSeries, Series};
     use std::ffi::{c_char, c_longlong, CStr, CString};
 
     /// List the catalog of the dss file
@@ -16,6 +20,7 @@ mod dss {
         let filename = Box::into_raw(filen);
         let mut cats = vec![];
         unsafe {
+            // the codes in this unsafe section is very similar to writing C code for DSS handling
             let mut dummy = 0;
             let exists = zfileName(filename as *mut c_char, 300, fnamec.as_ptr(), &mut dummy);
             zsetMessageLevel(0, 0);
@@ -47,5 +52,59 @@ mod dss {
             }
         }
         Ok(cats)
+    }
+
+    #[node_func]
+    fn load_series(
+        node: &mut NodeInner,
+        dssfile: String,
+        path: String,
+        name: String,
+    ) -> Result<(), String> {
+        let fnamec = CString::new(dssfile).unwrap();
+        let pathc = CString::new(path).unwrap();
+        let filen = Box::new([0; 300]);
+        let filename = Box::into_raw(filen);
+        let series: Series;
+        unsafe {
+            // the codes in this unsafe section is very similar to writing C code for DSS handling
+            let mut dummy = 0;
+            let exists = zfileName(filename as *mut c_char, 300, fnamec.as_ptr(), &mut dummy);
+            zsetMessageLevel(0, 0);
+            if exists == 0 {
+                return Err("DSS file does not exist".to_string());
+            }
+            let ifltab = Box::new([0; 250]);
+            let tab: *mut c_longlong = Box::into_raw(ifltab) as *mut _;
+            let status = hec_dss_zopen(tab, filename as *const c_char);
+            if status != (STATUS_OKAY as i32) {
+                return Err(format!("Error opening DSS file, {}", status));
+            }
+            let ts = zstructTsNew(pathc.as_ptr());
+            let status = ztsRetrieve(tab, ts, 0, 1, 0);
+            if status != (STATUS_OKAY as i32) {
+                return Err(format!(
+                    "Error retrieving timeseris from DSS file, {}",
+                    status
+                ));
+            }
+            let ts = *ts;
+            let val_count = ts.numberValues;
+            let ts_vals = std::slice::from_raw_parts(ts.floatValues, val_count as usize);
+            let values: Vec<ROption<f64>> = ts_vals
+                .iter()
+                .map(|v| {
+                    // zisMissingFloat returns int
+                    if zisMissingFloat(*v) == 1 {
+                        RNone
+                    } else {
+                        RSome(*v as f64)
+                    }
+                })
+                .collect();
+            series = MaskedSeries::floats(values).into();
+        }
+        node.set_series(&name, series);
+        Ok(())
     }
 }
