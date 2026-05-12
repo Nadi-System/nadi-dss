74530089de214a96d7f119f7876e4658322a59eb
Gaurav Atreya <allmanpride@gmail.com>
2026-05-11 23:47:30 -0400

    add save_series; update functions to take/return series directly

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,8 @@ mod dss {
     use nadi_core::abi_stable::std_types::{RNone, ROption, RSome};
     use nadi_core::nadi_plugin::{env_func, node_func};
     use nadi_core::prelude::*;
-    use nadi_core::timeseries::{MaskedSeries, Series};
-    use std::ffi::{c_char, c_longlong, CStr, CString};
+    use nadi_core::timeseries::{CompleteSeries, MaskedSeries, Series};
+    use std::ffi::{c_char, c_float, c_int, c_longlong, CStr, CString};
 
     /// List the catalog of the dss file
     ///
@@ -54,13 +54,8 @@ mod dss {
         Ok(cats)
     }
 
-    #[node_func]
-    fn load_series(
-        node: &mut NodeInner,
-        dssfile: String,
-        path: String,
-        name: String,
-    ) -> Result<(), String> {
+    #[env_func]
+    fn load_series(dssfile: String, path: String) -> Result<Series, String> {
         let fnamec = CString::new(dssfile).unwrap();
         let pathc = CString::new(path).unwrap();
         let filen = Box::new([0; 300]);
@@ -104,7 +99,76 @@ mod dss {
                 .collect();
             series = MaskedSeries::floats(values).into();
         }
-        node.set_series(&name, series);
+        Ok(series)
+    }
+
+    #[env_func(start_date = "1990-01-01", start_time = "12:00", delta_minute = 1440)]
+    fn save_series(
+        series: &Series,
+        dssfile: String,
+        path: String,
+        /// start date of the series
+        start_date: String,
+        /// start time of the series
+        start_time: String,
+        /// delta time in minutes
+        delta_minute: i64,
+    ) -> Result<(), String> {
+        let fnamec = CString::new(dssfile).unwrap();
+        let pathc = CString::new(path).unwrap();
+        let filen = Box::new([0; 300]);
+        let filename = Box::into_raw(filen);
+        if series.is_empty() {
+            return Err("empty Series".into());
+        }
+        unsafe {
+            // the codes in this unsafe section is very similar to writing C code for DSS handling
+            let mut dummy = 0;
+            let exists = zfileName(filename as *mut c_char, 300, fnamec.as_ptr(), &mut dummy);
+            zsetMessageLevel(0, 0);
+            let ifltab = Box::new([0; 250]);
+            let tab: *mut c_longlong = Box::into_raw(ifltab) as *mut _;
+            let status = hec_dss_zopen(tab, filename as *const c_char);
+            if status != (STATUS_OKAY as i32) {
+                return Err(format!("Error opening DSS file, {}", status));
+            }
+
+            let start_day = CString::new(start_date).unwrap();
+            let start_time = CString::new(start_time).unwrap();
+            let num_records = series.len() as c_int;
+            let missing = zmissingFlagFloat();
+            let values: Vec<f32> = match series {
+                Series::Complete(CompleteSeries::Floats(vals)) => {
+                    vals.into_iter().map(|v| *v as c_float).collect()
+                }
+                Series::Masked(MaskedSeries::Floats(vals), _) => vals
+                    .into_iter()
+                    .map(|v| v.map(|a| a as c_float).unwrap_or(missing))
+                    .collect(),
+                _ => {
+                    return Err(format!(
+                        "{} series not supported, only Floats supported for now",
+                        series.type_name()
+                    ))
+                }
+            };
+            let fvalues = values.as_slice().as_ptr() as *mut c_float;
+            let tss1 = zstructTsNewRegFloats(
+                pathc.as_ptr(),
+                fvalues,
+                num_records,
+                start_day.as_ptr(),
+                start_time.as_ptr(),
+                c"CFS".as_ptr(),
+                c"INST-VAL".as_ptr(),
+            );
+            (*tss1).timeIntervalSeconds = delta_minute as c_int;
+
+            let status = ztsStore(tab, tss1, 1);
+            if status != (STATUS_OKAY as i32) {
+                return Err(format!("Error saving timeseris to DSS file, {}", status));
+            }
+        }
         Ok(())
     }
 }
